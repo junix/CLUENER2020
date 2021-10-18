efae9a7040c3c8755d0e95469f6b802f870c37d5
王利军 <wanglj@yxt.com>
2021-10-18 18:08:16 +0800

    fix

--- pytorch_version/run_ner_crf.py
+++ pytorch_version/run_ner_crf.py
@@ -371,7 +371,6 @@ def load_and_cache_examples(args, task, tokenizer, data_type='train'):
 
 
 def convert_to_features(examples,
-                        label_list,
                         max_seq_length,
                         tokenizer,
                         cls_token_at_end=False,
@@ -389,18 +388,15 @@ def convert_to_features(examples,
             - True (XLNet/GPT pattern): A + [SEP] + B + [SEP] + [CLS]
         `cls_token_segment_id` define the segment id associated to the CLS token (0 for BERT, 2 for XLNet)
     """
-    label_map = {label: i for i, label in enumerate(label_list)}
     features = []
     for (ex_index, example) in enumerate(examples):
         if ex_index % 10000 == 0:
             logger.info("Writing example %d of %d", ex_index, len(examples))
-        tokens = tokenizer.tokenize(example.text_a)
-        label_ids = [label_map[x] for x in example.labels]
+        tokens = tokenizer.tokenize(example)
         # Account for [CLS] and [SEP] with "- 2".
         special_tokens_count = 2
         if len(tokens) > max_seq_length - special_tokens_count:
             tokens = tokens[: (max_seq_length - special_tokens_count)]
-            label_ids = label_ids[: (max_seq_length - special_tokens_count)]
 
         # The convention in BERT is:
         # (a) For sequence pairs:
@@ -421,59 +417,49 @@ def convert_to_features(examples,
         # used as as the "sentence vector". Note that this only makes sense because
         # the entire model is fine-tuned.
         tokens += [sep_token]
-        label_ids += [label_map['O']]
         segment_ids = [sequence_a_segment_id] * len(tokens)
 
         if cls_token_at_end:
             tokens += [cls_token]
-            label_ids += [label_map['O']]
             segment_ids += [cls_token_segment_id]
         else:
             tokens = [cls_token] + tokens
-            label_ids = [label_map['O']] + label_ids
             segment_ids = [cls_token_segment_id] + segment_ids
 
         input_ids = tokenizer.convert_tokens_to_ids(tokens)
         # The mask has 1 for real tokens and 0 for padding tokens. Only real
         # tokens are attended to.
         input_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
-        input_len = len(label_ids)
+        input_len = len(tokens)
         # Zero-pad up to the sequence length.
         padding_length = max_seq_length - len(input_ids)
         if pad_on_left:
             input_ids = ([pad_token] * padding_length) + input_ids
             input_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + input_mask
             segment_ids = ([pad_token_segment_id] * padding_length) + segment_ids
-            label_ids = ([pad_token] * padding_length) + label_ids
         else:
             input_ids += [pad_token] * padding_length
             input_mask += [0 if mask_padding_with_zero else 1] * padding_length
             segment_ids += [pad_token_segment_id] * padding_length
-            label_ids += [pad_token] * padding_length
 
         assert len(input_ids) == max_seq_length
         assert len(input_mask) == max_seq_length
         assert len(segment_ids) == max_seq_length
-        assert len(label_ids) == max_seq_length
         if ex_index < 5:
             logger.info("*** Example ***")
-            logger.info("guid: %s", example.guid)
             logger.info("tokens: %s", " ".join([str(x) for x in tokens]))
             logger.info("input_ids: %s", " ".join([str(x) for x in input_ids]))
             logger.info("input_mask: %s", " ".join([str(x) for x in input_mask]))
             logger.info("segment_ids: %s", " ".join([str(x) for x in segment_ids]))
-            logger.info("label_ids: %s", " ".join([str(x) for x in label_ids]))
 
-        inputs = [
+        features.append(
             {
                 "input_ids": torch.tensor([input_ids], dtype=torch.long),
                 "attention_mask": torch.tensor([input_mask], dtype=torch.long),
                 "labels": None,
                 'input_lens': torch.tensor([input_len], dtype=torch.long)
             }
-        ]
-        # features.append(InputFeatures(input_ids=input_ids, input_mask=input_mask, input_len=input_len,
-        #                               segment_ids=segment_ids, label_ids=label_ids))
+        )
     return features
 
 
